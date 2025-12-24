2288953c02ce31b493913fa9dc8bf20f2c59cb78
LAPTOP-GQA9U98Q\hamza <Hamza.Tabat@emsi-edu.ma>
2025-12-24 19:38:15 +0100

    sonarqube changes 6

--- src/main/java/org/sid/ebankingbackend/services/BankService.java
+++ src/main/java/org/sid/ebankingbackend/services/BankService.java
@@ -1,5 +1,6 @@
 package org.sid.ebankingbackend.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.sid.ebankingbackend.entities.BankAccount;
 import org.sid.ebankingbackend.entities.CurrentAccount;
 import org.sid.ebankingbackend.entities.SavingAccount;
@@ -10,28 +11,40 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@Slf4j
 public class BankService {
+
     @Autowired
     private BankAccountRepository bankAccountRepository;
-    public void consulter(){
-        BankAccount bankAccount=
-                bankAccountRepository.findById("0b36be78-8d5d-446b-9f20-37eadc9d3c3b").orElse(null);
-        if(bankAccount!=null) {
-            System.out.println("*****************************");
-            System.out.println(bankAccount.getId());
-            System.out.println(bankAccount.getBalance());
-            System.out.println(bankAccount.getStatus());
-            System.out.println(bankAccount.getCreatedAt());
-            System.out.println(bankAccount.getCustomer().getName());
-            System.out.println(bankAccount.getClass().getSimpleName());
-            if (bankAccount instanceof CurrentAccount currentaccount) {
-                System.out.println("Over Draft=>" + currentaccount.getOverDraft());
-            } else if (bankAccount instanceof SavingAccount savingaccount) {
-                System.out.println("Rate=>" + savingaccount.getInterestRate());
+
+    public void consulter() {
+        BankAccount bankAccount =
+                bankAccountRepository.findById("0b36be78-8d5d-446b-9f20-37eadc9d3c3b")
+                        .orElse(null);
+
+        if (bankAccount != null) {
+            log.info("*****************************");
+            log.info("Account ID       : {}", bankAccount.getId());
+            log.info("Balance          : {}", bankAccount.getBalance());
+            log.info("Status           : {}", bankAccount.getStatus());
+            log.info("Created At       : {}", bankAccount.getCreatedAt());
+            log.info("Customer Name    : {}", bankAccount.getCustomer().getName());
+            log.info("Account Type     : {}", bankAccount.getClass().getSimpleName());
+
+            if (bankAccount instanceof CurrentAccount currentAccount) {
+                log.info("OverDraft        : {}", currentAccount.getOverDraft());
+            } else if (bankAccount instanceof SavingAccount savingAccount) {
+                log.info("Interest Rate    : {}", savingAccount.getInterestRate());
             }
-            bankAccount.getAccountOperations().forEach(op -> {
-                System.out.println(op.getType() + "\t" + op.getOperationDate() + "\t" + op.getAmount());
-            });
+
+            bankAccount.getAccountOperations().forEach(op ->
+                    log.info("Operation => Type: {}, Date: {}, Amount: {}",
+                            op.getType(),
+                            op.getOperationDate(),
+                            op.getAmount())
+            );
+        } else {
+            log.warn("Bank account not found");
         }
     }
 }
